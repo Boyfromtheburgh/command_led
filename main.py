8077366d9383c079e8d2666bc32ad7a370ff6f3a
Joshua Gmys <98911948+Boyfromtheburgh@users.noreply.github.com>
2022-12-01 01:35:08 -0500

    quick fix

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import speech_recognition as sr
 from light_functions import *
 
 
-func_dict = {"red": turn_red, "green": turn_green, "purple" : turn_purple, "lights off" : lights_off, "Christmas" : christ1, "party" : party , "Brightness up"  : increaseBrightNess, "Brightness down" : lowerBrightness ,"Invert color" : invertColors, "steelers": steelers, "yellow" : yellow, "sky blue" : skyblue }
+func_dict = {"red": turn_red, "green": turn_green, "purple" : turn_purple, "lights off" : lights_off, "Christmas" : christ1, "party" : party , "Brightness up"  : increaseBrightNess, "Brightness down" : lowerBrightness ,"Invert color" : invertColors, "steelers": steelers, "yellow" : yellow, "sky blue" : skyblue, "orange" : orange }
 # Create NeoPixel object with appropriate configuration.
 strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
 # Intialize the library (must be called once before other functions).
